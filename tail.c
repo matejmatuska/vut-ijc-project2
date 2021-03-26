ff71adcca83b64979d5f229e251dc5cd66b91f53
Matej Matuska <75834032+matejmatuska@users.noreply.github.com>
2021-03-26 12:00:19 +0100

    Fix arg parsing in tail.c

--- tail.c
+++ tail.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MAX_LEN 512
 
@@ -24,6 +25,7 @@ char** buffer_create(unsigned size)
 }
 
 // prints buffer to stdout
+// prints in reverse order
 void buffer_print(char **buffer, unsigned n)
 {
     unsigned i = n - 1;
@@ -56,7 +58,7 @@ void tail(unsigned n, FILE *file)
     {
         fprintf(stderr, "Failed to allocate buffer!\n");
         //return EXIT_FAILURE;
-        return;
+        return; //TODO error handling
     }
 
     unsigned lines_read = 0;
@@ -89,47 +91,69 @@ void from_line(unsigned line_num, FILE *file)
 int main(int argc, char *argv[])
 {
     int n = 10;
-    FILE *file = stdin;
+    bool from_stdin = true;
+    bool start_mode = false;
 
-    if (argc == 2) // args should be a file
+    char *filename;
+
+    if (argc > 2 && argc <= 4)
     {
-        file = fopen(argv[1], "r");
-        if (file == NULL) 
+        if (strcmp("-n", argv[1]) == 0)
+        {
+            if (argv[2][0] == '+')
+                start_mode = true;
+
+            char *rest;
+            n = strtol(argv[2], &rest, 10);
+            if (*rest != '\0')
+            {
+                fprintf(stderr, "Error: Option -n requires integer argument!\n");
+                return EXIT_FAILURE;
+            }
+        }
+        else
+        {
+            fprintf(stderr, "Error: unexpected argument %s!\n", argv[1]);
+            return EXIT_FAILURE;
+        }
+
+        if (argc == 4)
+        {
+            from_stdin = false;
+            filename = argv [3];
+        }
+    }
+    else if (argc == 2)
+    {
+        from_stdin = false;
+        filename = argv[1];
+    }
+    else if (argc != 1)
+    {
+        fprintf(stderr, "Error: Too many arguments!\n");
+        return EXIT_FAILURE;
+    }
+
+    FILE *file;
+    if (from_stdin)
+        file = stdin;
+    else
+    {
+        file = fopen(filename, "r");
+        if (file == NULL)
         {
             perror("tail");
             return EXIT_FAILURE;
         }
-    } 
-    else if (argc == 3)  // args should be -n num
-    {
-        char *rest;
-        n = strtol(argv[2], &rest, 10);
-        if (*rest != '\0')
-        {
-            fprintf(stderr, "Error: Option -n requires integer argument!\n");
-            return EXIT_FAILURE;
-        }
-    }
-    else if (argc == 4) // args should be -n num file
-    {
-        char *rest;
-        n = strtol(argv[2], &rest, 10);
-        if (*rest != '\0')
-        {
-            fprintf(stderr, "Error: Option -n requires integer argument!\n");
-            return EXIT_FAILURE;
-        }
-
-        file = fopen(argv[3], "r");
-        if (file == NULL) 
-        {
-            perror("tail");
-            return EXIT_FAILURE;
-        }
     }
 
-    from_line(n, file);
-    //tail(n, file);
+    if (start_mode)
+        from_line(n, file);
+    else
+        tail(n, file);
+
+    if (!from_stdin)
+        fclose(file);
 
     return EXIT_SUCCESS;
 }

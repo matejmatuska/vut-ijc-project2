3379bf2b10a19dd61c0fb920868b7583c9cedfb6
Matej Matuska <75834032+matejmatuska@users.noreply.github.com>
2021-03-29 17:53:39 +0200

    Add htab_hash_func.c
    htab_hash_func.c contains our hashing function implementation

---
+++ htab_hash_func.c
@@ -0,0 +1,13 @@
+#include <inttypes.h>
+
+#include "htab.h"
+
+size_t htab_hash_function(htab_key_t str)
+{
+    uint32_t h = 0;     // musí mít 32 bitů
+    const unsigned char *p;
+    for(p = (const unsigned char*)str; *p != '\0'; p++)
+        h = 65599 * h + *p;
+
+    return h;
+}

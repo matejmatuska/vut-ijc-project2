b06d3b5388312c425d439b30fdea3b68b0156f30
Matej Matuska <75834032+matejmatuska@users.noreply.github.com>
2021-03-29 17:49:49 +0200

    htab_init.c: fixed error where .size and .arr_size were not initialized

--- htab_init.c
+++ htab_init.c
@@ -1,14 +1,23 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-#include "htab.h"
 #include "htab_internal.h"
+#include "htab.h"
 
 htab_t *htab_init(size_t n)
 {
-    if (n < 1)
+    if (n == 0)
     {
         //TODO args error hnadling
+        fprintf(stderr, "error: htab_init, size must be > 0, was: %lu", n);
+        return NULL;
     }
 
-    return malloc(sizeof(htab_t*) + n * sizeof(struct htab_item));
+    htab_t *t = malloc(sizeof(htab_t) + n * sizeof(struct htab_item*));
+    if (t != NULL)
+    {
+        t->size = 0;
+        t->arr_size = n;
+    }
+    return t;
 }
